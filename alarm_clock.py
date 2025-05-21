59524d5a6990f6e6cfb2cfc1598ba082345cb862
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-05-21 23:15:34 +0100

    Import the python libraries

---
+++ alarm_clock.py
@@ -0,0 +1,13 @@
+"""
+Advanced Alarm Clock Program
+A comprehensive alarm clock with customizable tones and snooze functionality.
+
+"""
+
+import time
+import datetime
+import threading
+import os
+import sys
+from typing import Optional, List, Dict
+
